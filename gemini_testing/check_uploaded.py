08cabaa41fc38ee18f9ad11e90ad92d9a338caa5
BismaBRJ <bismabrj@gmail.com>
2026-04-10 17:31:31 +0700

    testing out Gemini :D now just testing uploaded files for starters (of course there are none lol haven't used it yet)

---
+++ gemini_testing/check_uploaded.py
@@ -0,0 +1,28 @@
+# getting the API key
+# see secrets.json.example if you haven't set up secrets.json
+
+from pathlib import Path
+import json
+
+TOPDIR_PATH = Path(__file__).parent.parent
+SECRETS_PATH = TOPDIR_PATH / "secrets.json"
+
+with open(SECRETS_PATH, 'r', encoding="utf-8") as file:
+    SECRETS_JSON = json.load(file)
+
+GEMINI_API_KEY = SECRETS_JSON["GEMINI_API_KEY"]
+
+# now on to the AI stuff
+from google import genai
+
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+print("Uploaded files:")
+for f in client.files.list():
+    print("File name:")
+    print(f.name)
+    print("File:")
+    print(f)
+
+print("End of script.")
+
