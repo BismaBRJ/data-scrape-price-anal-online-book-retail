fd243cea144c8b11ad984475a62ff2dde1babb3d
BismaBRJ <bismabrj@gmail.com>
2026-06-14 17:22:46 +0700

    Started writing Gemini client calling loop, repeating if failed. Reconsidering how to store the results

--- data_enrichment/enrich_reviews.py
+++ data_enrichment/enrich_reviews.py
@@ -1,17 +1,20 @@
 # always set to False unless about to run
 USE_MODEL = False
+MODEL_NAME = "gemini-3.1-flash-lite-preview"
 
 # Imports
+print("Importing libraries etc. ...")
 from pathlib import Path # for path stuff
 import json
 import polars as pl # for dealing with CSV
-import base64
 from pydantic import BaseModel, Field
 import numpy as np # for ceil function
-import utils_enrich # for RNG
+import utils_enrich # for RNG, base64
 import time # for time.sleep()
 from google import genai
+import json
 import sys # for sys.exit()
+print("Imports done")
 
 # getting the API key
 # see secrets.json.example if you haven't set up secrets.json
@@ -36,8 +39,9 @@ old_df = pl.read_csv(old_csv_path, glob=False)
 old_df_cols = old_df.columns
 old_df_rowlen = len(old_df)
 
-# input images?? directly pull from CSV
-# TODO
+# get reference to thumbnails column on CSV (not deep copy!) 
+# this will be the start of building the new CSV
+images_df = old_df.select(pl.col("thumb_base64"))
 
 # prompt engineering and JSON output structuring
 
@@ -61,6 +65,7 @@ class BookInfo(BaseModel):
     sources: dict[str, str] = Field(description="Sources used and for what.")
 
 # respecting rate limits with random waiting times
+# code in utils_enrich.py
 
 n_imgs = old_df_rowlen
 MAX_REQS_PER_MIN = 15
@@ -70,7 +75,7 @@ init_avg_waiting_time = np.ceil(60/MAX_REQS_PER_MIN)
 seed_int = utils_enrich.get_datetime_seed()
 print("Random seed:", seed_int)
 
-(avg_waiting_time, waiting_times) = utils_enrich.get_floored_waiting_times(
+(rng, avg_waiting_time, waiting_times) = utils_enrich.get_floored_waiting_times(
         n_data=n_imgs,
         init_avg=init_avg_waiting_time,
         flooring_window_size=MAX_REQS_PER_MIN,
@@ -84,11 +89,57 @@ print(waiting_times)
 print("Total waiting time:", waiting_times.sum())
 
 # finally, data enrichment with Gemini
-# TODO
+
+"""
+new_df = images_df.with_columns(
+        # default empty values
+        year = pl.lit(0),
+        publisher = pl.lit(""),
+        edition = pl.lit(0),
+        pages = pl.lit(0),
+        hard = pl.lit(False), # assume paperback
+        extra_info_sources = pl.lit("") # string, to fit in CSV
+    )
+"""
 
 if USE_MODEL:
-    print("Calling API...")
+    print("Starting Gemini API client...")
     client = genai.Client(api_key=GEMINI_API_KEY)
+
+    for idx, row in enumerate(images_df.iter_rows()):
+        img_txt = row[0]
+        img_obj = utils_enrich.get_PIL_from_base64_text(img_txt)
+
+        print(f"Row {idx+1}")
+        print(f"Waiting {waiting_times[idx]} seconds...")
+        time.sleep(waiting_times[idx])
+        success = False
+        while (not success):
+            print("Calling API...")
+            response = client.models.generate_content(
+                model=MODEL_NAME,
+                contents=[MY_PROMPT, img_obj],
+                config={
+                    "response_mime_type": "application/json",
+                    "response_json_schema": BookInfo.model_json_schema()
+                }
+            )
+            if (response.text != None):
+                result_json = json.loads(response.text)
+                success = True
+            else:
+                # random cooldown before reattempt
+                random_waiting_time = rng.exponential(
+                        scale=init_avg_waiting_time,
+                        size=1
+                    )[0]
+                backup_waiting_time = (
+                        init_avg_waiting_time + random_waiting_time
+                    )
+                print(f"Failed, waiting {backup_waiting_time} seconds...")
+                time.sleep(backup_waiting_time)
+
+        print("Row {idx+1} success!")
 
     """
     my_imgs = []
@@ -96,7 +147,7 @@ if USE_MODEL:
         my_imgs.append(client.files.upload(file=img_path))
 
     response = client.models.generate_content(
-        model="gemini-3.1-flash-lite-preview",
+        model=MODEL_NAME
         contents=[MY_PROMPT, *my_imgs],
         config={
             "response_mime_type": "application/json",
